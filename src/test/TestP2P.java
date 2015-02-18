7c9dac58cd154835da59a969c96d37b50e306d70
Leo Schettini <ljsa@cin.ufpe.br>
2015-02-18 18:10:49 -0200

    nada

--- src/test/TestP2P.java
+++ src/test/TestP2P.java
@@ -24,6 +24,7 @@ public class TestP2P{
         if(tipo.equalsIgnoreCase("TCP")){
         	me = new UserTCP("leo", "172.22.46.112");
         	user = new UserTCP("<nome aqui>", "172.22.46.112");
+			//baixa
         } else {
         	me = new UserBTP("leo", "LocalHost");
         	user = new UserBTP("<nome aqui>", "LocalHost"); //destino
