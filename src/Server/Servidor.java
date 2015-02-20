28d6d52a83ecd714ae6442de20a9ca6010a46977
Leo Schettini <ljsa@cin.ufpe.br>
2015-02-20 04:02:12 -0200

    conexao cliente servidor (lado servidor) 80%

--- src/Server/Servidor.java
+++ src/Server/Servidor.java
@@ -1,10 +1,14 @@
 package Server;
 
-import java.net.InetAddress;
-import java.net.SocketException;
-import java.net.UnknownHostException;
+import java.io.IOException;
+import java.net.*;
 
+import application.address.model.Receiver;
+import application.address.model.Sender;
+import application.address.model.UserBTP;
+import messageCenter.ReceiverBTP;
 import protocol.Back;
+import protocol.BackTP;
 import protocol.Packet;
 import application.address.business.BusinessUser;
 import application.address.model.User;
@@ -12,29 +16,75 @@ import application.address.util.BeckSocket;
 
 public abstract class Servidor {
 	private BusinessUser controlUsers;
+	//identificacao de servidor p/ clientes (beckSocket)
 	private int port;
 	private InetAddress serverIp;
-	private Back protocol;
-	
-	public Servidor(String protocolo) throws UnknownHostException{
+
+	//thread
+	private boolean running;
+	private DatagramSocket receiverSocket;
+	private ReceiverBTP receiver;
+
+	//identificacao do servidor p/ clientes (conectar)
+	private final int PORT = 50000;
+
+	public Servidor(String protocolo) throws IOException {
 		
 		this.port = 8080;
 		this.controlUsers = new BusinessUser();
 		this.serverIp = InetAddress.getLocalHost();
-		
+
+		this.running = true;
+		this.receiverSocket = new DatagramSocket(PORT);
+		this.receiver = new ReceiverBTP(true, null);
+
 		if(protocolo.equalsIgnoreCase("tcp")){
-			//icializar o protocolo TCP
+			//icializar o receiver TCP
 			
 		}else{
-			//icializar o protocolo UDP
-			
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					while (true) {
+						byte[] udpBuffer = new byte[receiver.getBufferSize() + Packet.HEADER_SIZE];
+						DatagramPacket packet = new DatagramPacket(udpBuffer, udpBuffer.length);
+
+						while (running) {
+							//UDP receive
+							try {
+								receiverSocket.receive(packet);
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+
+							//cria pacote
+							Packet p = receiver.receivePacket(packet.getData());
+							//o username vem na primeira linha, o ip na segunda
+							String[] split = new String(receiver.getBytes(p.getData(), p.getDataLength())).split("\n");
+
+							//conecta ao servidor se usuario ainda n estiver no repositorio de beckSockets
+							if (!controlUsers.exist(split[1])) {
+								conectarAoServidor(new UserBTP(split[0], split[1]));
+							} else {
+								//usuario ja conectado
+							}
+							//envia "shake" com datagramSocket
+
+						}
+					}
+				}
+			}).start();
 		}
 	}
 	
-	public void conectarAoServidor(User user) throws SocketException{
-		
-		BeckSocket beck = new BeckSocket(port, user.getIp());
-//		protocol.se
+	public void conectarAoServidor(User user) { //recebe porta?? e a thread /\ incrementa port?
+		controlUsers.addUser(user);
+		try {
+			BeckSocket beck = new BeckSocket(port, user.getIp());
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+
 	}
 	
 	public void redirecionarPorta(){
